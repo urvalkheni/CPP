6dc6a9e6a8f6fff1f473724f1efba8f12a63b66c
Urval Kheni <kheniurval777@gmai.com>
2025-01-10 17:11:16 +0530

    while loop added

---
+++ Program/whileloop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /* Syntax for while loop
+    while(conditon){
+        C++ Statements;
+    }
+    */
+   int i = 0;
+   while(i<=40){
+    cout<<i<<endl;
+    i++;
+   }
+   
+    return 0;
+}
