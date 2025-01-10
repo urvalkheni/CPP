81fdb42f1b11d2a7c4838787bdfe0771af163d8d
Urval Kheni <kheniurval777@gmai.com>
2025-01-10 17:26:52 +0530

    Table Added

---
+++ Program/tablewithloop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 6;
+    int i = 1;
+    do {
+        cout<<a<<" x "<<i<<" = "<<a * i<< endl;
+        i++;
+    } while (i <= 10);
+    return 0;
+}
