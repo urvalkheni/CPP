8abd402346ccfc7b862e789535351df59bb9a6fa
Urval Kheni <kheniurval777@gmai.com>
2025-01-18 18:09:13 +0530

    Final Commit

---
+++ Program/ArrayandPointer/greet.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Print a message
+    cout << "Hello, World!" << endl;
+
+    // Take user input
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+
+    // Greet the user
+    cout << "Nice to meet you, " << name << "!" << endl;
+
+    return 0;
+}
