d754c48ccee9a3b3becb664e6c71e0bf36176cfd
Urval Kheni <kheniurval777@gmai.com>
2025-02-20 17:24:35 +0530

    Final Commit

---
+++ Program/basics/numswap.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cout<<"Enter Two Numbers :: ";cin>>num1>>num2;
+    cout<<"Before swap Number 1 is  :: "<<num1<<endl<<"Before swap Number 2 is :: "<<num2<<endl;
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
+    cout<<"After swap Number 1 is  :: "<<num1<<endl<<"After swap Number 2 is :: "<<num2<<endl;
+    return 0;
+}
