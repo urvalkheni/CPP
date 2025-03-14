eaab58ab800393259ded6d38690d8b7526ce9a71
Urval Kheni <kheniurval777@gmail.com>
2025-03-14 16:07:54 +0530

    Final Commit

--- Program/Letters/A.cpp
+++ Program/Letters/A.cpp
@@ -1,14 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     int n;
     cout<<"Enter The Number :: ";cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            cout<<"("<<i<<","<<j<<") ";   
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= 2 * n; j++) {
+            if (j==n-i || j==n+i || (i==n/2 && j>n-i&&j<n+i)) {
+                cout<<"*";
+            } else {
+                cout <<" ";
+            }
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
