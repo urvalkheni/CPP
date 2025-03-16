ffd02acc850db63ece2cfe3deb10a2e09b83c307
Urval Kheni <kheniurval777@gmail.com>
2025-03-16 17:59:34 +0530

    Final Commit

--- Program/Letters/K.cpp
+++ Program/Letters/K.cpp
@@ -1,15 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     int n;
-    cout<<"Enter The Number n :: ";
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            cout<<"* ";            
-        }
-        cout<<endl;
+    cout << "Enter The Number n :: ";
+    cin >> n;
+
+    if (n < 3) {  // Ensuring minimum size for visibility
+        cout << "Enter a value greater than or equal to 3.";
+        return 0;
     }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n / 2 + 1; j++) {  
+            if (j == 0 || i == j + n / 4 || i + j == n - n / 4 - 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
