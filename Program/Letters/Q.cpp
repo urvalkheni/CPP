ffcf6d17dbe08d9e47fcab0c9ff72314944d56e2
Urval Kheni <kheniurval777@gmail.com>
2025-03-19 10:19:34 +0530

    All 26 Letter Completed

--- Program/Letters/Q.cpp
+++ Program/Letters/Q.cpp
@@ -6,11 +6,9 @@ int main(){
     cout<<"Enter The Number :: ";cin>>n;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            if(i==1 || j==1 || i==n || j==n){
-                if((i==j || i+j==n+1)){
-                cout<<"  ";
-                }
-                else cout<<"* ";
+            if(((i==1 || i==n-1 )&& j!=n) || ((j==1 || j==n-1) && i!=n) || (i==j && i>=(n/2)+1)){
+                
+                cout<<"* ";
             }
             else cout<<"  ";
         }
