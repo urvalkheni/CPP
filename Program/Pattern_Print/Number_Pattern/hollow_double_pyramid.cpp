d11cb7b557302782a43724d116adb06d67725744
Urval <kheniurval777@gmail.com>
2025-11-22 22:19:41 +0530

    2/5 commit of program

---
+++ Program/Pattern_Print/Number_Pattern/hollow_double_pyramid.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+using namespace std;
+int main(){ int n; if(!(cin>>n)||n<=0) return 0; for(int i=1;i<=n;++i){ for(int s=0;s<n-i;++s) cout<<"  "; for(int j=1;j<=2*i-1;++j){ if(j==1||j==2*i-1||i==n) cout<<i<<' '; else cout<<"  "; } cout<<'\n'; } for(int i=n-1;i>=1;--i){ for(int s=0;s<n-i;++s) cout<<"  "; for(int j=1;j<=2*i-1;++j){ if(j==1||j==2*i-1||i==n) cout<<i<<' '; else cout<<"  "; } cout<<'\n'; } return 0; }
