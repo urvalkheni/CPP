ad22b5a38f479f3a9eeb73477abcca40d514f768
Urval <kheniurval777@gmail.com>
2025-12-09 17:25:17 +0530

    2/4 commit of Program

--- Program/basics/numSwapXOR.cpp
+++ Program/basics/numSwapXOR.cpp
@@ -3,11 +3,20 @@ using namespace std;
 
 int main(){
     int num1,num2;
-    cout<<"Enter Two Numbers :: ";cin>>num1>>num2;
-    cout<<"Before swap Number 1 is  :: "<<num1<<endl<<"Before swap Number 2 is :: "<<num2<<endl;
-    num1 = num1 ^ num2;
-    num2 = num1 ^ num2;
-    num1 = num1 ^ num2;
-    cout<<"After swap Number 1 is  :: "<<num1<<endl<<"After swap Number 2 is :: "<<num2<<endl;
+    cout << "Enter Two Numbers :: ";
+    if (!(cin >> num1 >> num2)) {
+        cout << "Invalid input." << endl;
+        return 1;
+    }
+
+    cout << "Before swap Number 1 is  :: " << num1 << endl << "Before swap Number 2 is :: " << num2 << endl;
+
+    if (num1 != num2) {
+        num1 = num1 ^ num2;
+        num2 = num1 ^ num2;
+        num1 = num1 ^ num2;
+    }
+
+    cout << "After swap Number 1 is  :: " << num1 << endl << "After swap Number 2 is :: " << num2 << endl;
     return 0;
 }
