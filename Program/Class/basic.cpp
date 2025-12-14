b175687c597d1b1c2a6fa079884aa174181ac62f
Urval <kheniurval777@gmail.com>
2025-12-14 13:53:19 +0530

    2/3 commit of Program modification

--- Program/Class/basic.cpp
+++ Program/Class/basic.cpp
@@ -1,29 +1,60 @@
 #include<iostream>
+#include<string>
 using namespace std;
+
 class Employee{
-    private :
-    int a,b,c;
-    public :
-    int d,e;
-    void setData(int a1,int b1,int c1);
-    void getData(){
-        cout<<"The Value of a is :: "<<a<<endl;
-        cout<<"The Value of b is :: "<<b<<endl;
-        cout<<"The Value of c is :: "<<c<<endl;
-        cout<<"The Value of d is :: "<<d<<endl;
-        cout<<"The Value of e is :: "<<e<<endl;
-    }
+    private:
+        int empID;
+        string name;
+        double salary;
+        int workHours;
+        
+    public:
+        int department;
+        int age;
+        
+        // Constructor
+        Employee() : empID(0), name(""), salary(0.0), workHours(0), department(0), age(0) {}
+        
+        void setData(int id, string n, double sal, int hours) {
+            empID = id;
+            name = n;
+            salary = (sal >= 0) ? sal : 0;
+            workHours = (hours >= 0) ? hours : 0;
+        }
+        
+        void getData(){
+            cout << "\n=== Employee Details ==="<<endl;
+            cout << "Employee ID: " << empID << endl;
+            cout << "Name: " << name << endl;
+            cout << "Salary: $" << salary << endl;
+            cout << "Work Hours: " << workHours << " hrs/week" << endl;
+            cout << "Department: " << department << endl;
+            cout << "Age: " << age << endl;
+        }
+        
+        double calculateMonthlyPay() {
+            return salary / 12.0;
+        }
+        
+        bool isOvertime() {
+            return workHours > 40;
+        }
 };
-void Employee :: setData (int a1,int b1,int c1){
-    a = a1;
-    b = b1;
-    c = c1;
-}
+
 int main(){
+    cout << "=== Employee Management System ==="<<endl;
+    
     Employee urval;
-    urval.e = 60;
-    urval.d = 45;
-    urval.setData(1,2,3);
+    urval.age = 25;
+    urval.department = 101;
+    urval.setData(1001, "Urval Kheni", 60000.0, 45);
+    
     urval.getData();
+    
+    cout << "\n=== Additional Info ==="<<endl;
+    cout << "Monthly Pay: $" << urval.calculateMonthlyPay() << endl;
+    cout << "Overtime: " << (urval.isOvertime() ? "Yes" : "No") << endl;
+    
     return 0;
 }
